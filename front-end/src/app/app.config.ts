a03dc10fbe7304bfaaf0641ae8126f7053b81e5f
FelipeLV12 <felipe.lopesv2006@gmail.com>
2025-11-05 12:07:01 -0300

    Update app.config.ts

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,21 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { meuhttpInterceptor } from './auth/http-interceptor.service';
-import { FormsModule } from '@angular/forms';
-import { NgSelectModule } from '@ng-select/ng-select'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+import { routes } from './app.routes';
+import { meuHttpInterceptor } from './auth/http-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(), 
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([meuhttpInterceptor])),
+    provideHttpClient(withInterceptors([meuHttpInterceptor])),
+    
     importProvidersFrom(
       FormsModule,
+      ReactiveFormsModule,
+      MatInputModule,    
+      MatButtonModule,
+      MatCardModule,
+      MatFormFieldModule,
       NgSelectModule
     )
   ]
