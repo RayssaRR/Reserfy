d6565e6e5f5e873110fa21af7ff6625a9d5213ae
FelipeLV12 <felipe.lopesv2006@gmail.com>
2025-11-05 12:38:27 -0300

    Update app.routes.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,24 +1,27 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/layout/home.component/home.component';
-import { LoginComponent } from './components/layout/login.component/login.component';
+import { HomeComponent } from './components/layout/home/home.component';
+import { LoginComponent } from './components/layout/login/login.component';
 import { RegisterComponent } from './components/layout/register.component/register.component';
 import { DashboardComponent } from './components/layout/dashboard.component/dashboard.component';
 import { PrincipalComponent } from './components/layout/principal.component/principal.component';
 import { NgModule } from '@angular/core';
-import { GuardGuard } from './auth/guard.guard'; 
-
+import { GuardGuard } from './auth/guard.guard';
+import { ContactComponent } from './pages/contact/contact.component'; 
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
-  {path: 'user',  component: PrincipalComponent, canActivate:[GuardGuard], children:[
-  { path: 'dashboard', component: DashboardComponent}
-   ] },
-   { path: '**', redirectTo: 'login' }
+  {
+      path: 'contato',
+      component: ContactComponent
+  },
+  {path: 'user', component: PrincipalComponent, canActivate:[GuardGuard], children:[
+    { path: 'dashboard', component: DashboardComponent }
+  ]},
+  { path: '**', redirectTo: 'login' }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
