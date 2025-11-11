8b2bc8db95f4558d5e9a5399d3f864c5dcc563e6
FelipeLV12 <felipe.lopesv2006@gmail.com>
2025-11-10 21:19:45 -0300

    Create home-page.component.ts

---
+++ front-end/src/app/components/home/homepage/home-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']); 
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']); 
+  }
+}
